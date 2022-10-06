6ac86e8fb94020356da8b479937169617d5c4dd5
Daniel Briquez <102300060+Briqz23@users.noreply.github.com>
2022-10-06 18:52:16 -0300

    activity.py ok

--- src/domain/entities/activity.py
+++ src/domain/entities/activity.py
@@ -8,15 +8,35 @@ class Activity:
     description: str
     initialDate: str
     finalDate: str
+    activityType: str
+    speakers: str
+    acceptSubscriptionUntilDate: bool
+    location: str
+    remoteRoomUrl: str
+    acceptSubscription: bool
+    #devem ser declarados como string ou um int? 
+    maxParticipants: int
 
-    def __init__(self, title, type, code, description, initialDate, finalDate):
+
+
+
+    def __init__(self, title, type, code, description, initialDate, 
+                finalDate, activityType, speakers, acceptSubscriptionUntilDate, 
+                location, remoteRoomUrl, acceptSubscription, maxParticipants):
+
         self.title = title
         self.type = type
         self.code = code
         self.description = description
         self.initialDate = initialDate
         self.finalDate = finalDate
-
+        self.activityType = activityType
+        self.speakers = speakers
+        self.acceptSubscriptionUntilDate = acceptSubscriptionUntilDate
+        self.location = location
+        self.remoteRoomUrl = remoteRoomUrl
+        self.acceptSubscription = acceptSubscription
+        self.maxParticipants = maxParticipants
 
 
 
