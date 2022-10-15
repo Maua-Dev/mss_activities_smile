5fe90ba64803bcd95a1857fcb8cc7e2b15376824
Daniel Briquez <102300060+Briqz23@users.noreply.github.com>
2022-10-15 00:53:26 -0300

    controller

---
+++ src/modules/delete_activity/delete_activity_controller.py
@@ -0,0 +1,34 @@
+import code
+from src.helpers.http_models import HttpRequest, HttpResponse
+from src.modules.delete_activity.delete_activity_usecase import DeleteActivityUsecase
+from src.helpers.errors import MISSING_FIELD
+from src.helpers.http_models import HttpRequest, HttpResponse, Created, BadRequest, InternalServerError
+class DeleteActivityController:
+    def __init__(self, usecase: DeleteActivityUsecase):
+        self.usecase = usecase
+        # self.DeleteActivityUsecase = usecase / dá na mesma?
+
+    def __call__(self, request: HttpRequest) -> HttpResponse:
+        try:
+            activity = request.body
+
+            activity_code = activity.get('code')
+            if not activity_code or activity_code == '':
+                raise MISSING_FIELD("code")
+
+            if not activity.get('initialDate'):
+                raise MISSING_FIELD("initialDate")
+
+            self.usecase(activity)
+          
+            return Created()
+
+        except MISSING_FIELD as e:
+            return BadRequest(body=e.args[0])
+        except:
+            return InternalServerError(body='Internal Server Error')
+
+
+
+
+        
