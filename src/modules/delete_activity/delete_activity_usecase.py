91e43b08442f88b18e678bbc1581484963c623a6
Daniel Briquez <102300060+Briqz23@users.noreply.github.com>
2022-10-15 02:18:41 -0300

    get activity controller

---
+++ src/modules/delete_activity/delete_activity_usecase.py
@@ -0,0 +1,12 @@
+from src.domain.entities.activity import Activity
+from src.domain.repositories.activity_repository_interface import IActivityRepository
+
+
+class DeleteActivityUsecase:
+        def __init__(self, activity_repository:IActivityRepository)->None:
+            self.activity_repository = activity_repository
+        
+        def __call__(self, activity) -> bool:
+            activity = self.activity_repository.delete_activity(activity)
+            return activity
+            #precisa de validação?
