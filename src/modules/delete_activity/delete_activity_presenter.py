f7f17048bdd0419565e183b9facb02a072cdfe26
Daniel Briquez <102300060+Briqz23@users.noreply.github.com>
2022-10-14 23:57:50 -0300

    presenter

---
+++ src/modules/delete_activity/delete_activity_presenter.py
@@ -0,0 +1,19 @@
+from src.domain.repositories.activity_repository_interface import ActivityRepository
+from src.helpers.http_models import HttpResponse
+from src.infra.repositories.activity_repository_mock import ActivityRepositoryMock
+from src.modules.delete_activity.delete_activity_usecase import DeleteActivityUsecase
+from src.helpers.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+async def lambda_handler(event, context):
+    repo = ActivityRepositoryMock()
+    usecase = DeleteActivityUsecase(repo)
+    controller = DeleteActivityUsecase(usecase)
+
+    httpRequest = LambdaHttpRequest(data=event)
+    response = await controller(httpRequest)
+    HttpResponse = LambdaHttpResponse(  
+        status_code=response.status_code,
+        body=response.body, 
+        headers=response.headers
+        )
+        
+
