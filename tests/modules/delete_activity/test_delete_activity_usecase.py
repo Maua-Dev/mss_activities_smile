a62ceaec43432ebf4e8569fdcfbaa5fddf171104
Caio <ctoledo@pieracciani.com.br>
2022-10-25 08:45:31 -0300

    corrigindo nome do método

--- tests/modules/delete_activity/test_delete_activity_usecase.py
+++ tests/modules/delete_activity/test_delete_activity_usecase.py
@@ -3,32 +3,15 @@ from src.domain.entities.activity import Activity
 from src.helpers.errors import MISSING_FIELD
 from src.infra.repositories.activity_repository_mock import ActivityRepositoryMock
 from src.modules.delete_activity.delete_activity_usecase import DeleteActivityUsecase
-from  src.helpers.errors import MISSING_FIELD
+from src.helpers.errors import MISSING_FIELD
+
 
 class Test_DeleteActivityUsecase:
 
     def test_delete_activity_usecase(self):
         repo = ActivityRepositoryMock()
         usecase = DeleteActivityUsecase(repo=repo)
-        res = usecase(code = "Código 1")
-       
+        res = usecase(code="Código 1")
+
         assert repo.activities[0] != "Código 1"
         assert res
-        
-
-
-
-
-
-
-
-
-
-
-    '''
-    to do:
-
-    definir diferentes erros em src/helpers/errors.py 
-    testar aqui
-    
-    '''
