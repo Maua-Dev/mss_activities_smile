24a79c223289de7d9c309002326a734742a64343
Daniel Briquez <dbriquez.devmaua@gmail.com>
2022-11-01 17:26:39 -0300

    speaker's validation

--- src/domain/entities/speaker.py
+++ src/domain/entities/speaker.py
@@ -1,5 +1,3 @@
-# Façam validação dos atributos
-
 class Speaker:
     name: str
     bio: str
@@ -9,3 +7,25 @@ class Speaker:
         self.name = name
         self.bio = bio
         self.company = company
+
+
+        @staticmethod
+        def validate_name(name: str)->bool:
+        
+            if name == None:
+                return False
+
+            if type(name) != str:
+                return False
+
+
+        @staticmethod
+        def validate_bio(bio: str)->bool:
+        
+            if bio == None:
+                return False
+
+            if type(bio) != str:
+                return False
+
+        
